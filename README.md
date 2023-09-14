1f26ddc8206105ddd780860e83586967a333f734
Chris M <chrismok97@gmail.com>
2023-09-14 16:45:52 -0700

    Added all content on home page following the Figma webframe

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# bandom
+# Bandom
+
+A collaboration with a friend who is studying UI/UX. This project is based on her project for a Music Merchandise Mobile Application. I will be bringing it to life using vanilla JavaScript, HTML and CSS.
+
+This single page application does not use any backend or server to retrieve content from but rather uses the window url to change the content and simulate navigating through the application.
--- index.js
+++ index.js
@@ -1,16 +1,63 @@
 const contentContainer = document.getElementById('content');
 
 const loadHomePage = () => {
+    // Set Document title and clear any content
     document.title = 'Bandom | Home';
+    contentContainer.innerHTML= '';
+
+    // Create the Newest Styles div
+    const newestStyles = document.createElement('div');
+    newestStyles.setAttribute('id', 'newest-styles')
+    newestStyles.innerHTML = '<h1>Our Newest Styles</h1>'
+
+    // Create Exclusives div
+    const exclusives = document.createElement('div');
+    exclusives.setAttribute('id', 'exclusives')
+    exclusives.innerHTML = '<h1>Bandom Exclusives</h1>'
+
+    // Create Wishlist div
+    const wishlist = document.createElement('div');
+    wishlist.setAttribute('id', 'wishlist');
+    wishlist.innerHTML = '<h2>Wishlist</h2>'
+
+    const wishlistItemContainer = document.createElement('div');
+    wishlistItemContainer.setAttribute('id', 'wishlist-container');
+    wishlist.appendChild(wishlistItemContainer)
+
+    // Create 3 Wishlist Item divs
+    for (let i = 1; i <= 3; i++) {
+        const wishlistItem = document.createElement('div');
+        wishlistItem.setAttribute('class', 'wishlist-item');
+        wishlistItem.textContent = `ITEM ${i}`;
+        wishlistItemContainer.appendChild(wishlistItem)
+    }
+    
+    // Create the Catalogue div
+    const catalogue = document.createElement('div');
+    catalogue.setAttribute('id', 'catalogue');
+    catalogue.innerHTML = '<h1><a href="#/catalogue">Catalogue</a></h1>'
+
+    // Add all created divs to the Content div
+    contentContainer.appendChild(newestStyles)
+    contentContainer.appendChild(exclusives)
+    contentContainer.appendChild(wishlist)
+    contentContainer.appendChild(catalogue)
 };
 
 const loadAccountPage = () => {
     document.title = 'Bandom | Account';
+    contentContainer.innerHTML= '';
 };
 
 
 const loadCartPage = () => {
     document.title = 'Bandom | Cart';
+    contentContainer.innerHTML= '';
+};
+
+const loadCataloguePage = () => {
+    document.title = 'Bandom | Catalogue';
+    contentContainer.innerHTML= '';
 };
 
 const handleRouteChange = () => {
@@ -25,7 +72,9 @@ const handleRouteChange = () => {
         case '/cart':
             loadCartPage();
             break
-    }
+        case '/catalogue':
+            loadCataloguePage();
+    };
 };
 
 window.addEventListener('hashchange', handleRouteChange);
